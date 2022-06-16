14c52f9062c3868399b63bbad311540859d4a050
Martijn van Houwelingen <99066439@mydavinci.nl>
2022-06-16 12:00:46 +0200

    B3W2L3.2

--- php/week2/B3W2L3/welcome.php
+++ php/week2/B3W2L3/welcome.php
@@ -5,11 +5,11 @@
     <?php if ($_SERVER["REQUEST_METHOD"] == "POST") { ?>
         <?php
         echo '<h1>De ingevulde gegevens zijn:</h1>';
-        echo 'Naam: ' . $_POST['name'];
-        echo nl2br("\r\nEmailadres: " . $_POST['email']);
+        echo 'Naam: ' . htmlspecialchars($_POST['name']);
+        echo nl2br("\r\nEmailadres: " . htmlspecialchars($_POST['email']));
         ?>
     <?php } else { ?>
-        <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        <form method="post" action="">
         <label for="fname">Name:</label><br>
         <input type="text" id="name" name="name"><br>
         <label for="lname">Mail:</label><br>
